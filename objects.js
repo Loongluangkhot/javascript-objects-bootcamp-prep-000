b6ed47197b7e87133b9aaadb939f834ae0e5b07c
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-14 15:59:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {'Maroon 5':'She Will Be Loved'}
+var playlist = {}
 
 function updatePlaylist(playlist,artist,song) {
   playlist[artist] = song
@@ -9,7 +9,3 @@ function removeFromPlaylist(playlist,artist) {
   delete playlist[artist]
   return playlist
 }
-
-console.log(updatePlaylist(playlist,'Coldplay','Yellow'))
-
-console.log(removeFromPlaylist(playlist,'Maroon 5'))
